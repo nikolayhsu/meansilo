dba3024124c91d564e1ed22aed98a0af5775a9cb
Ricardo Russon <ricardo@buzzcloud.com.au>
2016-03-08 14:51:01 +1000

    Added login.js

---
+++ app/public/controllers/login.js
@@ -0,0 +1,40 @@
+var pmApp = angular.module( "pmApp" , [] );
+
+pmApp.controller('loginController' , ['$scope','$http',function($scope , $http){
+
+	$scope.userLogin = function() {
+	
+		$http.post('/login' , $scope.user).success(function(response){
+	
+			if ( response.logedin !== undefined && response.logedin == true ) {
+		
+				window.location.href = '/search';
+		
+			} else {
+		
+				// login failure
+				$scope.loginAlertStatus = "";
+				$("#loginAlertBox").fadeIn();
+				
+			}
+		
+		});
+	
+	};
+	
+}]);
+
+pmApp.directive('pmEnter', function () {
+    return function ($scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+            	$scope.$apply(function (){
+                    $scope.$eval(attrs.pmEnter);
+                });
+                event.preventDefault();
+            } else {
+            	$("#loginAlertBox").fadeOut();
+            }
+        });
+    };
+});
